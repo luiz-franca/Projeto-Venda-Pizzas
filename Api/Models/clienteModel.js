8f7d9863ba4bd3a434eb2adc69b677562dd60ee0
GustavoNote <rocha.gustavo@escolar.ifrn.edu.br>
2025-02-01 07:02:31 -0300

    updadete cliente

--- Api/Models/clienteModel.js
+++ Api/Models/clienteModel.js
@@ -22,7 +22,16 @@ class ClienteModel{
                      VALUES ('${this.nomeCliente}', '${this.telefone}','${this.endereco}','${this.email}', '${this.senha}');`;
         return await queryExecute(sql);
     }
+    async updateCliente(updadeIdCliente){
+        if (!this.idCliente) throw new Error("ID do cliente é necessário para atualização.");
+
+        const sql = `UPDATE TbCliente 
+                     SET nomeCliente='${this.nomeCliente}', telefone='${this.telefone}', email='${this.email}', senha='${this.senha}'
+                     WHERE idCliente=${updadeIdCliente};`;
+        return await queryExecute(sql);
+    }
 }
+
 // (async () => {
 //     try {
 //         const novoCliente = new ClienteModel(null, 'Aslan Lilinho', 'telefone do papai', 'casa do papai','email@testew' ,'senhaPapaiSabe');
@@ -32,3 +41,18 @@ class ClienteModel{
 //         console.error("Erro:", error.message);
 //     }
 // })();
+(async ()=>{
+    try{
+        const cliente = new ClienteModel(null, 'Gustavo', '20202020','casa', 'email@teste.com', 'prodasd');
+        // const check = cliente.insertCliente();
+        // const getcliente = await ClienteModel.getTodosClientes();
+        // console.log(getcliente)
+        cliente.nomeCliente = "Alsan";
+        const check = await cliente.updateCliente(1)
+        console.log(check)
+
+
+    }catch(error){
+        console.error(error)
+    }
+})();
